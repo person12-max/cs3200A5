5cfdbe44113733a285621075e1380f967b2f5009
randomusername0 <hwang.t@husky.neu.edu>
2020-12-03 17:24:45 -0500

    asdfsdf

--- daos/questions-dao.js
+++ daos/questions-dao.js
@@ -1,4 +1,4 @@
-const questionsModel = require('../models/questions/questions-models.js')
+const questionsModel = require('../models/questions/questions-model.js')
 const findAllQuestions = () => questionsModel.find()
 const findQuestionById = (questionId) => questionsModel.findById(questionId)
 const findQuestionsForQuiz = (quizId) => questionsModel.find(quizId)
--- daos/quizzes-dao.js
+++ daos/quizzes-dao.js
@@ -1,4 +1,4 @@
-const quizzesModel = require('../models/quizzes/quizzes-models.js')
+const quizzesModel = require('../models/quizzes/quizzes-model.js')
 const findAllQuizzes = () => quizzesModel.find()
 const findQuizById = (quizId) => quizzesModel.findById(quizId)
 module.exports = { findAllQuizzes, findQuizById }
