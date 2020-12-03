7ce021a5bcb381a00b2b7fb2bee40e9ca50e154c
randomusername0 <hwang.t@husky.neu.edu>
2020-12-03 17:05:10 -0500

    added stuff

---
+++ daos/questions-dao.js
@@ -0,0 +1,5 @@
+const questionsModel = require('../models/questions/questions-model.js')
+const findAllQuestions = () => questionsModel.find()
+const findQuestionById = (questionId) => questionsModel.findById(questionId)
+const findQuestionsForQuiz = (quizId) => questionsModel.find(quizId)
+module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz}
